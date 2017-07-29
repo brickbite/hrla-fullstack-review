bd4fbbc3ffb0a2e141dcbb6195cecd8eac598af6
Hugo Dolemieux <hugodolemieux@verizon.net>
2017-07-29 14:25:01 -0700

    [MASTER INIT]

--- server/router/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-const questionsRouter = require('./questionsRouter');
-
-router.use('/questions', questionsRouter);
-
-module.exports = router;
--- server/router/questionsRouter.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const controller = require('../controllers/questionsController');
-
-router.get('/fetch', controller.fetchQuestions);
-
-router.post('/create', controller.createQuestion);
-
-module.exports = router;
